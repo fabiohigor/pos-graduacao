1d23946051c156f7d5da8efab388919c2e7ec27c
Fabio <fabioec.2010@gmail.com>
2025-07-06 19:10:39 -0400

    feat: Aula 7 - Persistência com Panache
    - Configurado Panache, JSON-B e banco H2
    - Implementada entidade Pessoa
    - Criada consulta personalizada por anoNascimento

---
+++ 2 - Desenvolvimento de Aplicações Back-End com Spring Boot e Quarkus/unipds/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO Pessoa (id, nome, anoNascimento) VALUES
+(1, 'João', 1992),
+(2, 'Maria', 1992),
+(3, 'José', 1983),
+(4, 'Ana', 2001),
+(5, 'Pedro', 1988);
+
+Alter sequence pessoa_id_seq restart with 6;
