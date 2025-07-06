3c1a0ee67cdf1c7d87073ee9daa3dff0a473c0c2
Fabio <fabioec.2010@gmail.com>
2025-07-05 21:27:42 -0400

    Concluída a aula 6 do módulo 1 - Quarkus, da disciplina Desenvolvimento de aplicações back-end com Spring e Quarkus, conteúdo estudado: @Fallback, @CircuitBreaker e @Timeout

--- 2 - Desenvolvimento de Aplicações Back-End com Spring Boot e Quarkus/unipds/src/main/java/br/com/fabiohigor/StarWarsService.java
+++ 2 - Desenvolvimento de Aplicações Back-End com Spring Boot e Quarkus/unipds/src/main/java/br/com/fabiohigor/StarWarsService.java
@@ -4,15 +4,28 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(baseUri = "https://swapi.info/api")
 public interface StarWarsService {
 
+
+    public static final String MSG_ERROR = "{\"error\": \"Service is currently unavailable. Please try again later.\"}";
+
     @GET
     @Path("starships")
     @Produces(MediaType.APPLICATION_JSON)
+    @Timeout(value = 3000L)
+    @Fallback(fallbackMethod = "getStarshipsFallback")
+    @CircuitBreaker(requestVolumeThreshold = 5, failureRatio = 0.5, delay = 1000L, successThreshold = 5)
     String getStarships();
+
+    default String getStarshipsFallback() {
+        return MSG_ERROR;
+    }
 
     @GET
     @Path("films")
