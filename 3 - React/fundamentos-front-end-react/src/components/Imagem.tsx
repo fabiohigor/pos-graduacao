f3f2fe2a47230da50db9ade7b4a710e95ae4dfa2
Fabio <fabioec.2010@gmail.com>
2025-08-02 14:35:17 -0400

    Aula 0 concluida

--- 3 - React/fundamentos-front-end-react/src/components/Imagem.tsx
+++ 3 - React/fundamentos-front-end-react/src/components/Imagem.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Imagem = () => (
   
     <Image
-     className="dark:invert"
+      className="dark:invert"
       src="/next.svg"
       alt="Next.js Logo"
       width={180}
