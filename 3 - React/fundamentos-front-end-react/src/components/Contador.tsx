cb1de4d6060c61edafb986598bbd5162361df01e
Fabio <fabioec.2010@gmail.com>
2025-08-02 16:36:46 -0400

    Nivel 1 - concluindo aula de eventos

--- 3 - React/fundamentos-front-end-react/src/components/Contador.tsx
+++ 3 - React/fundamentos-front-end-react/src/components/Contador.tsx
@@ -1,11 +1,58 @@
 "use client";
 import { useState } from "react";
+import { Button } from "./Button";
 
 export const Contador = () => {
-    const [contador] = useState(0);
-    
-    return <div>
+    const [contador, setContador] = useState(0);
+    const [coisa, setCoisa] = useState("");
+
+    return <div className="flex flex-col gap-y-2">
         <h2 className="text-xl">Contador</h2>
         <p>Número atual: {contador}</p>
+        <div className="flex gap-x-2">
+            <Button
+                onClick={() => {
+                    setContador((c) => c - 3);                
+                }}
+            >
+                -3
+            </Button>
+            <Button
+                onClick={() => {
+                    setContador(contador - 1);
+                }}
+            >
+                -1
+            </Button>
+            <Button
+                onClick={() => {
+                    setContador(contador + 1);
+                }}
+            >
+                +1
+            </Button>
+            <Button
+                onClick={() => {
+                    setContador((c) => c + 3);
+                }}
+            >
+                +3
+            </Button>
+            
+        </div>
+        <div className="flex gap-x-2">
+            <input className="border border-gray-500 px-4 py-1 rounded"
+                value={coisa} onChange={(e) => {
+                setCoisa(e.target.value);
+            }} />
+            <Button
+                onClick={() => {
+                    setCoisa("");
+                }}
+            >
+                Limpar
+            </Button>
+
+        </div>
     </div>
 };
